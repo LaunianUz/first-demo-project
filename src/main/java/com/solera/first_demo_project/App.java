fa7700433068bd6627be7cd14ea9dc27b08a83f8
OAVega <112970551+OAVega@users.noreply.github.com>
2022-09-06 12:39:41 -0500

    Add files via upload

---
+++ src/main/java/com/solera/first_demo_project/App.java
@@ -0,0 +1,16 @@
+package com.solera.first_demo_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" );
+        int a = 3;
+        int b = 4;
+        
+        System.out.println(a+b);
+    }
+}
